8933e181cee0382b3e8145c64156b86f5700d2b4
Milan-Kurpershoek <1105163@hr.nl>
2025-09-23 14:58:16 +0200

    Add tracker.php page and update VCS mapping
    Introduced a new tracker.php file with basic HTML structure and PHP includes. Updated .idea/vcs.xml to explicitly map the project directory to Git version control.

---
+++ tracker.php
@@ -0,0 +1,36 @@
+<?php
+// PHP here
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://kit.fontawesome.com/1fe3729de2.js" crossorigin="anonymous"></script> <!-- Icon Library -->
+    <link rel="stylesheet" href="css/style.css"> <!-- Global styling -->
+    <link rel="stylesheet" href="css/index.css"> <!-- Page specific styling -->
+    <title>Novara Health • Home</title>
+</head>
+<body>
+
+<header>
+    [Logo]
+    <div class="empty-div"></div>
+    Hey, [name]!
+</header>
+
+<main>
+  <p>Hello World</p>
+</main>
+
+<nav>
+    <?php
+    include_once "includes/nav.php";
+    ?>
+</nav>
+
+</body>
+</html>
