c3eb7e51922cd6c78eaa16d7d89321ee14217771
Milan-Kurpershoek <1105163@hr.nl>
2025-09-24 11:43:48 +0200

    Add mood tracker form and tracker.css styling
    Introduced a new tracker.css file for form styling and updated tracker.php to include a mood tracker form with checkboxes. The form is styled using the new CSS, and a placeholder for an energy tracker is included as commented code.

--- tracker.php
+++ tracker.php
@@ -11,7 +11,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <script src="https://kit.fontawesome.com/1fe3729de2.js" crossorigin="anonymous"></script> <!-- Icon Library -->
     <link rel="stylesheet" href="css/style.css"> <!-- Global styling -->
-    <link rel="stylesheet" href="css/index.css"> <!-- Page specific styling -->
+    <link rel="stylesheet" href="css/tracker.css"> <!-- Page specific styling -->
     <title>Novara Health • Home</title>
 </head>
 <body>
@@ -19,11 +19,53 @@
 <header>
     [Logo]
     <div class="empty-div"></div>
-    Hey, [name]!
 </header>
 
 <main>
   <p>Hello World</p>
+
+    <section class="form-group">
+        <form action="" method="post">
+            <div class="field">
+                <p>Mood</p>
+
+                <label class="label" for="one">1</label>
+                <input class="input" id="one" type="checkbox" name="one" value="<?= $one ?>"/>
+
+                <label class="label" for="two">2</label>
+                <input class="input" id="two" type="checkbox" name="two" value="<?= $two ?>"/>
+
+                <label class="label" for="three">3</label>
+                <input class="input" id="three" type="checkbox" name="three" value="<?= $three ?>"/>
+
+                <label class="label" for="four">4</label>
+                <input class="input" id="four" type="checkbox" name="four" value="<?= $four ?>"/>
+
+                <label class="label" for="five">5</label>
+                <input class="input" id="five" type="checkbox" name="five" value="<?= $five ?>"/>
+            </div>
+
+<!--            <div class="field">-->
+<!--                <p>energy</p>-->
+<!--                <label class="label" for="one">1</label>-->
+<!--                <input class="input" id="one" type="checkbox" name="one" value="--><?php //= $one ?><!--"/>-->
+<!---->
+<!--                <label class="label" for="two">2</label>-->
+<!--                <input class="input" id="two" type="checkbox" name="two" value="--><?php //= $two ?><!--"/>-->
+<!---->
+<!--                <label class="label" for="three">3</label>-->
+<!--                <input class="input" id="three" type="checkbox" name="three" value="--><?php //= $three ?><!--"/>-->
+<!---->
+<!--                <label class="label" for="four">4</label>-->
+<!--                <input class="input" id="four" type="checkbox" name="four" value="--><?php //= $four ?><!--"/>-->
+<!---->
+<!--                <label class="label" for="five">5</label>-->
+<!--                <input class="input" id="five" type="checkbox" name="five" value="--><?php //= $five ?><!--"/>   -->
+<!--            </div>-->
+        </form>
+    </section>
+
+
 </main>
 
 <nav>
